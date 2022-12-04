e6ce6c1e38ea2b464a135e32c19d1c4900487248
Alex Goodman <alexthegoodman@gmail.com>
2022-12-04 12:39:36 -0500

    s3 region

--- packages/common-server/src/adapters/S3.ts
+++ packages/common-server/src/adapters/S3.ts
@@ -6,6 +6,7 @@ const s3 = s3Adapter({
       accessKeyId: process.env.S3_ACCESS_KEY_ID,
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
     },
+    region: process.env.S3_REGION,
   },
   bucket: process.env.S3_BUCKET,
 });
