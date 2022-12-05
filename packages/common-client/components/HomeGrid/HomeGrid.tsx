a2a56e0de4703de04d76cce3041cc817d5b2b465
Alex Goodman <alexthegoodman@gmail.com>
2022-12-05 15:35:24 -0500

    home grid

--- packages/common-client/components/HomeGrid/HomeGrid.tsx
+++ packages/common-client/components/HomeGrid/HomeGrid.tsx
@@ -3,33 +3,40 @@ import * as React from "react";
 import styles from "./HomeGrid.module.scss";
 
 import { HomeGridProps } from "./HomeGrid.d";
+import Link from "next/link";
 
 const HomeGrid: React.FC<HomeGridProps> = () => {
   return (
     <section className={styles.homeGrid}>
       <div className={styles.homeGridInner}>
         <div className={styles.left}>
-          <div className={styles.gridItem}>
-            <span>StreamDoc</span>
-            <p>Google-Compliant Writing Assistant</p>
+          <a
+            href="https://streamdoc.app"
+            target="_blank"
+            className={styles.gridItem}
+          >
+            <div className={styles.itemContent}>
+              <span>StreamDoc</span>
+              <p>Google-Compliant Writing Assistant</p>
+            </div>
             <div className={styles.icon}>
               <img src="/svg/bigLink.svg" />
             </div>
-          </div>
+          </a>
         </div>
         <div className={styles.right}>
-          <div className={styles.gridItem}>
+          <Link href="/blog" className={styles.gridItem}>
             <span>10 Ways to be Active in Poetry</span>
             <div className={styles.icon}>
               <img src="/svg/arrowRight.svg" />
             </div>
-          </div>
-          <div className={styles.gridItem}>
+          </Link>
+          <Link href="/blog" className={styles.gridItem}>
             <span>Promoting Your Book with Libraries</span>
             <div className={styles.icon}>
               <img src="/svg/arrowRight.svg" />
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </section>
