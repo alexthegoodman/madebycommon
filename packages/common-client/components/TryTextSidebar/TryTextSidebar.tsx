4b2d9181b7cbd2bfc9f6c4765808ebc7ffa2d800
Alex Goodman <alexthegoodman@gmail.com>
2022-12-15 13:42:50 -0500

    try commontext sidebar

--- packages/common-client/components/TryTextSidebar/TryTextSidebar.tsx
+++ packages/common-client/components/TryTextSidebar/TryTextSidebar.tsx
@@ -3,9 +3,27 @@ import * as React from "react";
 import styles from "./TryTextSidebar.module.scss";
 
 import { TryTextSidebarProps } from "./TryTextSidebar.d";
+import Image from "next/image";
 
 const TryTextSidebar: React.FC<TryTextSidebarProps> = () => {
-  return <>TryTextSidebar</>;
+  return (
+    <section className={styles.tryTextSidebar}>
+      <div className={styles.tryTextSidebarInner}>
+        <h5>Try CommonText</h5>
+        <Image
+          className={styles.featureImage}
+          src="/images/tempCommonText2.jpg"
+          alt="CommonText app"
+          width={350}
+          height={200}
+        />
+        <p>Surface Relevant Information As You Type</p>
+        <a className={styles.btn} target="_blank" href="https://commontext.app">
+          Try Free
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default TryTextSidebar;
