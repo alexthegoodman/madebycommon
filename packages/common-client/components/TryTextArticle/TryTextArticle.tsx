0cf9a2aa0fc49697f1925d94dc3506606df4d970
Alex Goodman <alexthegoodman@gmail.com>
2022-12-15 13:57:51 -0500

    try text article

--- packages/common-client/components/TryTextArticle/TryTextArticle.tsx
+++ packages/common-client/components/TryTextArticle/TryTextArticle.tsx
@@ -5,7 +5,17 @@ import styles from "./TryTextArticle.module.scss";
 import { TryTextArticleProps } from "./TryTextArticle.d";
 
 const TryTextArticle: React.FC<TryTextArticleProps> = () => {
-  return <>TryTextArticle</>;
+  return (
+    <section className={styles.tryTextArticle}>
+      <div className={styles.tryTextArticleInner}>
+        <h5 className={styles.headline}>Try CommonText</h5>
+        <p className={styles.copy}>Surface Relevant Information As You Type</p>
+        <a className={styles.btn} target="_blank" href="https://commontext.app">
+          Try Free
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default TryTextArticle;
