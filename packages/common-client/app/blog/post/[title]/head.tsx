3d96c20f6bc317a78e1fd705ab8e3ccdb7e9011c
Alex Goodman <alexthegoodman@gmail.com>
2023-02-01 19:27:29 -0500

    add thumbnail to og image

--- packages/common-client/app/blog/post/[title]/head.tsx
+++ packages/common-client/app/blog/post/[title]/head.tsx
@@ -9,6 +9,24 @@ const Head = async ({ params }) => {
       <title>{post.meta.title}</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content={post.meta.description} />
+      <meta property="og:type" content="website" />
+
+      {/* <meta property="og:url" content="https://deeptype.app/"/> */}
+      <meta property="og:title" content={post.meta.title} />
+      <meta property="og:description" content={post.meta.description} />
+      <meta
+        property="og:image"
+        content={post.meta?.image?.sizes?.largeThumbnail?.url}
+      />
+      <meta property="twitter:card" content="summary_large_image" />
+      {/* <meta property="twitter:url" content="https://deeptype.app/"/> */}
+      <meta property="twitter:title" content={post.meta.title} />
+      <meta property="twitter:description" content={post.meta.description} />
+      <meta
+        property="twitter:image"
+        content={post.meta?.image?.sizes?.largeThumbnail?.url}
+      />
+
       <link rel="icon" href="/favicon.ico" />
     </>
   );
