d065baaba6bac1613de94344a15b3a16c8598bc5
Alex Goodman <alexthegoodman@gmail.com>
2023-02-17 19:20:13 -0500

    add google analytics

--- packages/common-client/app/layout.tsx
+++ packages/common-client/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.scss";
 import { Lato } from "@next/font/google";
 import PageHeader from "../components/PageHeader/PageHeader";
 import PageFooter from "../components/PageFooter/PageFooter";
+import Script from "next/script";
 
 const lato = Lato({ subsets: ["latin"], weight: ["100", "300", "400", "700"] });
 
@@ -18,6 +19,21 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-KFMMQJBWE5"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-KFMMQJBWE5');
+        `}
+      </Script>
+
       <body>
         <PageHeader />
         {children}
